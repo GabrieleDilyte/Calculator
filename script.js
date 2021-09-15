189fdbd5baef137e0ae0000ef17478ef0919a46f
Gabrielė Dilytė <GDI@tiatechnology.com>
2021-09-15 21:03:04 +0300

    files edited

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const btnSum = document.querySelector("#sum");
-const btnSub = document.querySelector("#sub");
-const btnDiv = document.querySelector("#div");
-const btnMul = document.querySelector("#mul");
-const btnMod = document.querySelector("#mod");
-const btnClear = document.querySelector("#clear");
-let num1 = 0;
-let num2 = 0;
-let result = null;
+const allButtons = document.querySelectorAll(".buttons button");
 
-function getInputNumbers() {
-  num1 = Number(document.querySelector("#firstNumber").value);
-  num2 = Number(document.querySelector("#secondNumber").value);
-}
-
-function calculate(option) {
+function calculate(option, num1, num2) {
   switch (option) {
     case "sum":
       return num1 + num2;
@@ -25,53 +12,23 @@ function calculate(option) {
       return num1 * num2;
     case "mod":
       return num1 % num2;
+    case "clear":
+      document.querySelector("#firstNumber").value = 0;
+      document.querySelector("#secondNumber").value = 0;
+      return 0;
     default:
       console.log("There is no such option");
   }
 }
 
-function logResult(result) {
-  if (isNaN(result)) {
-    document.querySelector("#errorMessage").innerHTML =
-      "Please enter number type variables";
-  } else {
-    document.querySelector("#errorMessage").innerHTML = null;
+allButtons.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    //Getting input numbers
+    const num1 = Number(document.querySelector("#firstNumber").value);
+    const num2 = Number(document.querySelector("#secondNumber").value);
+    //Calculating based on button function
+    const result = calculate(event.target.id, num1, num2);
+    //Logging result
     document.querySelector("#result").innerHTML = result;
-  }
-}
-
-btnSum.addEventListener("click", (event) => {
-  getInputNumbers();
-  result = calculate("sum");
-  logResult(result);
-});
-
-btnSub.addEventListener("click", (event) => {
-  getInputNumbers();
-  result = calculate("sub");
-  logResult(result);
-});
-
-btnDiv.addEventListener("click", (event) => {
-  getInputNumbers();
-  result = calculate("div");
-  logResult(result);
-});
-
-btnMul.addEventListener("click", (event) => {
-  getInputNumbers();
-  result = calculate("mul");
-  logResult(result);
-});
-
-btnMod.addEventListener("click", (event) => {
-  getInputNumbers();
-  result = calculate("mod");
-  logResult(result);
-});
-
-btnClear.addEventListener("click", (event) => {
-  document.querySelector("#firstNumber").value = 0;
-  document.querySelector("#secondNumber").value = 0;
-  logResult(0);
+  });
 });
